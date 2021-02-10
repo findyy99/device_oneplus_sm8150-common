737d0f3bdd714ad9435bdbcdb72142e3984a68d8
Matheus <mattvieira123@gmail.com>
2021-02-09 21:57:00 -0300

    KangOS - Bring!

--- DeviceSettings/src/com/yaap/device/DeviceSettings/TouchscreenGestureSettings.java
+++ DeviceSettings/src/com/yaap/device/DeviceSettings/TouchscreenGestureSettings.java
@@ -37,8 +37,8 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 
-import com.android.internal.derp.hardware.LineageHardwareManager; // Need FWB support
-import com.android.internal.derp.hardware.TouchscreenGesture; // Need FWB support
+import com.android.internal.kangos.hardware.LineageHardwareManager; // Need FWB support
+import com.android.internal.kangos.hardware.TouchscreenGesture; // Need FWB support
 
 import com.yaap.device.DeviceSettings.R;
 
